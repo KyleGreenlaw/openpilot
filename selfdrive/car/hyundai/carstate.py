e70e2438d9e8c224a4b7db571cd3057c0eeb5fc1
Kyle Greenlaw <Kyroapps@gmail.com>
2020-03-30 21:42:37 -0300

    Update carstate.py

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -316,11 +316,8 @@ class CarState(CarStateBase):
     self.esp_disabled = cp.vl["TCS15"]['ESC_Off_Step']
     self.park_brake = cp.vl["CGW1"]['CF_Gway_ParkBrakeSw']
 
-    self.main_on = 7 > cp_cam.vl["LKAS11"]["CF_Lkas_LdwsSysState"] != 0 if not self.no_radar else \
-					cp.vl['EMS16']['CRUISE_LAMP_M']
-
-    self.acc_active = 7 > cp_cam.vl["LKAS11"]["CF_Lkas_LdwsSysState"] != 0 if not self.no_radar else \
-                                       (cp.vl["LVR12"]['CF_Lvr_CruiseSet'] != 0)
+    self.main_on = 1
+    self.acc_active =1
     self.pcm_acc_status = int(self.acc_active)
 
     self.v_wheel_fl = cp.vl["WHL_SPD11"]['WHL_SPD_FL'] * CV.KPH_TO_MS
