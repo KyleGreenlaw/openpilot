6dc8317bcda74451e9210b6f3e8f39aadae5978d
Kyle Greenlaw <Kyroapps@gmail.com>
2020-05-15 16:00:56 -0300

    Update carstate.py

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -57,7 +57,7 @@ class CarState(CarStateBase):
     #TODO: find pedal signal for EV/HYBRID Cars
     if self.CP.carFingerprint in FEATURES["2020_electric"]:
       ret.gas = cp.vl["E_EMS11"]['Accel_Pedal_Pos'] / 100
-      if cp.vl["E_EMS11"]["Accel_Pedal_Pos"] =! 0:
+      if cp.vl["E_EMS11"]["Accel_Pedal_Pos"] != 0:
         ret.gasPressed = true
       else: 
         ret.gasPressed = false
