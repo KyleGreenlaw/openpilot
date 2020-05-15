9632c904bf37dcb7e1c183a59e2d3cb5d2c33b22
Kyle Greenlaw <Kyroapps@gmail.com>
2020-05-15 16:51:03 -0300

    Update carstate.py

--- selfdrive/car/hyundai/carstate.py
+++ selfdrive/car/hyundai/carstate.py
@@ -233,7 +233,7 @@ class CarState(CarStateBase):
         ("Accel_Pedal_Pos", "E_EMS11", 0)
       ]
       checks += [
-        ("E_EMS11", 20)
+        ("E_EMS11", 10)
       ]
     else:
       signals += [
