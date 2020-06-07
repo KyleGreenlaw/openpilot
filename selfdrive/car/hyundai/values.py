8bdde408ec2649bdc2ad44476969892d12f9fddb
Kyle Greenlaw <Kyroapps@gmail.com>
2020-06-07 10:48:01 -0300

    Update values.py

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -151,7 +151,6 @@ FEATURES = {
   "use_cluster_gears": [CAR.ELANTRA, CAR.KONA, CAR.ELANTRA_GT_I30],     # Use Cluster for Gear Selection, rather than Transmission
   "use_tcu_gears": [CAR.KIA_OPTIMA, CAR.SONATA_2019],                                    # Use TCU Message for Gear Selection
   "use_elect_gears": [CAR.KIA_OPTIMA_H, CAR.KONA_EV],  # Use TCU Message for Gear Selection
-  "use_elect_ems": [],  # Use E_EMS11 Message for Gas and Brake for Hybrid/ELectric
 }
 
 DBC = {
